f001e901a2b78b3f7caecd3d8666d7733e3677ac
wuehr1999 <wuehr1999>
2026-03-21 14:04:32 +0100

    poc

---
+++ public/script/sensor.js
@@ -0,0 +1,33 @@
+function update() {
+    const http = new XMLHttpRequest();
+    console.log(window.location.hostname);
+    http.open(
+        'GET', String('http://127.0.0.1:5000/temperature')
+    );
+    http.setRequestHeader("Access-Control-Allow-Origin", "*")
+    http.send();
+    http.onload = () => updateData(JSON.parse(http.responseText));
+}
+
+function updateData(data) {
+   var datasets = [];
+   
+   var layout = {
+       showlegend: true,
+       title: { text: 'Temperature' },
+       yaxis: {
+       title: { text: "Celsius" }
+       }
+   };
+  
+   var dataset = []
+   
+   for(var d in data) {
+       dataset.push({
+	       name: data[d].dev_id,
+               y: data[d].celsius,
+               type: 'scatter'
+       });
+   }
+   Plotly.newPlot('plot', dataset, layout);
+}
