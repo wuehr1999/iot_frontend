ffdf5b66453f7a144ce0f5ce5a58abbd5de22be0
wuehr1999 <wuehr1999>
2026-04-05 18:52:21 +0200

    update api path

--- public/script/sensor.js
+++ public/script/sensor.js
@@ -2,7 +2,7 @@ function update() {
     const http = new XMLHttpRequest();
     console.log(window.location.hostname);
     http.open(
-        'GET', String('http://' + window.location.hostname + ':5000/temperature')
+        'GET', String('http://' + window.location.hostname + ':5000/api/v1/temperature')
     );
     http.setRequestHeader("Access-Control-Allow-Origin", "*")
     http.send();
